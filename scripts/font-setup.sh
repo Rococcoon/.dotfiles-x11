4b7f65412537a772e307563125066215a984752a
lulu <lulu@lbrm.ht.home>
2024-09-17 12:37:46 -0700

    attempt new script for font downloads

--- scripts/font-setup.sh
+++ scripts/font-setup.sh
@@ -3,46 +3,51 @@
 # Set up script for fonts
 # By: LBRM
 
-# Define font directory and font URLs
-FONT_DIR="$HOME/.local/share/fonts"
-MONONOKI_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Mononoki.zip"
-SYMBOLS_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/NerdFontsSymbolsOnly.zip"
-FONT_AWESOME_URL="https://use.fontawesome.com/releases/v5.15.4/fontawesome-free-5.15.4-desktop.zip"
-MONONOKI_ZIP="$FONT_DIR/Mononoki.zip"
-SYMBOLS_ZIP="$FONT_DIR/Symbols.zip"
-FONT_AWESOME_ZIP="$FONT_DIR/fontawesome.zip"
+# Set the directories
+NERD_FONTS_DIR="$HOME/.fonts/nerd-fonts"
+MDI_FONTS_DIR="$HOME/.fonts/material-design-icons"
+LOCAL_FONTS_DIR="$HOME/.local/share/fonts"
 
-# Create font directory if it doesn't exist
-mkdir -p "$FONT_DIR"
+# URLs for the fonts
+MONONOKI_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Mononoki.zip"
+MONONOKI_ZIP="Mononoki.zip"
+MDI_FONT_URL="https://github.com/Templarian/MaterialDesign-Webfont/archive/refs/heads/master.zip"
+MDI_ZIP="material-design-icons.zip"
 
-# Download the Mononoki Nerd Font
+# Create necessary directories if they don't exist
+mkdir -p "$NERD_FONTS_DIR" "$MDI_FONTS_DIR" "$LOCAL_FONTS_DIR"
+
+# Download Mononoki Nerd Font zip file
 echo "Downloading Mononoki Nerd Font..."
-curl -fLo "$MONONOKI_ZIP" "$MONONOKI_URL"
+curl -fLo "$NERD_FONTS_DIR/$MONONOKI_ZIP" "$MONONOKI_FONT_URL"
 
-# Download the Symbols Nerd Font
-echo "Downloading Symbols Nerd Font..."
-curl -fLo "$SYMBOLS_ZIP" "$SYMBOLS_URL"
+# Unzip Mononoki Nerd Font
+echo "Unzipping Mononoki Nerd Font..."
+unzip -o "$NERD_FONTS_DIR/$MONONOKI_ZIP" -d "$NERD_FONTS_DIR/Mononoki"
 
-# Download Font Awesome
-echo "Downloading Font Awesome..."
-curl -fLo "$FONT_AWESOME_ZIP" "$FONT_AWESOME_URL"
+# Copy Mononoki font files to ~/.local/share/fonts
+echo "Copying Mononoki fonts to ~/.local/share/fonts..."
+cp "$NERD_FONTS_DIR/Mononoki"/*.ttf "$LOCAL_FONTS_DIR/"
 
-# Extract the downloaded fonts
-echo "Extracting Mononoki Nerd Font..."
-unzip -o "$MONONOKI_ZIP" -d "$FONT_DIR"
-echo "Extracting Symbols Nerd Font..."
-unzip -o "$SYMBOLS_ZIP" -d "$FONT_DIR"
-echo "Extracting Font Awesome..."
-unzip -o "$FONT_AWESOME_ZIP" -d "$FONT_DIR"
+# Download Material Design Icons zip file
+echo "Downloading Material Design Icons font..."
+curl -fLo "$MDI_FONTS_DIR/$MDI_ZIP" "$MDI_FONT_URL"
 
-# Remove the zip files after extraction
-echo "Cleaning up..."
-rm "$MONONOKI_ZIP"
-rm "$SYMBOLS_ZIP"
-rm "$FONT_AWESOME_ZIP"
+# Unzip Material Design Icons
+echo "Unzipping Material Design Icons..."
+unzip -o "$MDI_FONTS_DIR/$MDI_ZIP" -d "$MDI_FONTS_DIR/MaterialDesign"
 
-# Refresh the font cache
-echo "Refreshing font cache..."
+# Copy Material Design Icons font to ~/.local/share/fonts
+echo "Copying Material Design Icons font to ~/.local/share/fonts..."
+cp "$MDI_FONTS_DIR/MaterialDesign/fonts"/*.ttf "$LOCAL_FONTS_DIR/"
+
+# Update the font cache
+echo "Updating font cache..."
 fc-cache -fv
 
-echo "Mononoki, Symbols Nerd Fonts, and Font Awesome installed successfully."
+# Cleanup zip files
+echo "Cleaning up zip files..."
+rm "$NERD_FONTS_DIR/$MONONOKI_ZIP"
+rm "$MDI_FONTS_DIR/$MDI_ZIP"
+
+echo "Mononoki Nerd Font and Material Design Icons font installed successfully!"
