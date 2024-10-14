981ea45119212109890f88d5b1aac2af04fefd8e
Rococoon <spacedangles@gmail.com>
2024-10-13 21:20:52 -0700

    add tssever to nvim

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -2,8 +2,9 @@
 
 -- Load Lua LSP configuration
 require('lsp.css_ls')
-require('lsp.eslint_ls')
+-- require('lsp.eslint_ls')
 require('lsp.html_ls')
 require('lsp.json_ls')
 require('lsp.lua_ls')
 require('lsp.markdown_ls')
+require('lsp.tsserver')
--- config/nvim/lua/lsp/tsserver.lua
+++ config/nvim/lua/lsp/tsserver.lua
@@ -1,25 +1,27 @@
 -- Update this with the actual path to the TypeScript Language Server
-local tsserver_path = "~/.local/share/lsp/vscode-language-server/node_modules/typescript/lib/tsserver.js"
-tsserver_path = vim.fn.expand(tsserver_path)
+local lsp_path = "/home/lulu/.nvm/versions/node/v22.9.0/bin/typescript-language-server"
+lsp_path = vim.fn.expand(lsp_path)
 
 -- Command to start the TypeScript Language Server
-local cmd = { "node", tsserver_path }
+local cmd = { lsp_path, "--stdio" }
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" }, -- Attach to JavaScript and TypeScript file types
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+		"javascript",
+		"javascriptreact",
+	}, -- Attach to JavaScript and TypeScript file types
 	callback = function(args)
 		vim.lsp.start({
 			name = "typescript-language-server",
 			cmd = cmd,
-			root_dir = vim.fs.root(args.buf, { "package.json", "tsconfig.json", ".git" }), -- Define root directory
-			settings = {
-				typescript = {
-					tsserver = {
-						-- TypeScript-specific settings can be added here
-						diagnostics = true,
-					},
-				},
-			},
+			root_dir = vim.fs.root(args.buf, {
+				"package.json",
+				"tsconfig.json",
+				".git",
+			}), -- Define root directory
 		})
 	end,
 })
